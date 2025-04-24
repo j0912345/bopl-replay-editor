dd3f8810f7276dac2b5e46eac193bd09a760b771
!j0 <josiahdo07@outlook.com>
2025-04-23 18:41:52 -0700

    setup plugin

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace replay_editor
+{
+    [BepInPlugin("com.jo912345.replayEditor", "replayEditor", PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
