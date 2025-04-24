5acc9d41c27b12d476f47d32ed093073615633bb
!j0 <josiahdo07@outlook.com>
2025-04-23 18:49:17 -0700

    fix the name, get harmony

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,23 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
-namespace replay_editor
+namespace replayEditor
 {
     [BepInPlugin("com.jo912345.replayEditor", "replayEditor", PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        private readonly Harmony harmony = new Harmony("com.jo912345.hitboxVisualizePlugin");
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+            Logger.LogError($"{PluginInfo.PLUGIN_NAME} has been unloaded. (if you see this when starting the game, it's likely that `HideManagerGameObject = false` in `BepInEx.cfg`. please enable it!)");
         }
     }
 }
